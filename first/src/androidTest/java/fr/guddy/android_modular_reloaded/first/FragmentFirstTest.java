fb0dde7ab2529b978e05bd07ec6a355610f6aa0c
Romain Rochegude <romain.rochegude@gmail.com>
2017-09-27 20:37:19 +0200

    add missing import

--- first/src/androidTest/java/fr/guddy/android_modular_reloaded/first/FragmentFirstTest.java
+++ first/src/androidTest/java/fr/guddy/android_modular_reloaded/first/FragmentFirstTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class FragmentFirstTest {
